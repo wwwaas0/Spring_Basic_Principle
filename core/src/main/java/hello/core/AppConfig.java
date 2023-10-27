332e5c60a5e1c8413c56d039aa8650a69b326fb9
wwwaas0 <bjm040364@gmail.com>
2023-10-27 19:20:50 +0900

    [sytle] 스프링 컨테이너 설명 추가

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,6 +15,14 @@ import org.springframework.context.annotation.Configuration;
 /*
     각 인터페이스에 어떤 구현체를 주입할지 정하는 클래스
  */
+
+/*
+    스프링 컨테이너는 @Configuration이 붙은 클래스를 설정 정보로 사용
+    여기서 @Bean이 붙은 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록
+    이렇게 스프링 컨테이너에 등록된 객체를 스프링 빈이라고 한다.
+    또한, 스프링 빈은 @Bean이 붙은 메서드 명을 스프링 빈의 이름으로 사용. 물론 @Beam(name = "바꿀 이름")으로 이름 바꿀 수 있음
+    ex) 'memberService', 'orderService'
+ */
 @Configuration //이 클래스는 DI 컨테이너라는 뜻
 public class AppConfig {
 
